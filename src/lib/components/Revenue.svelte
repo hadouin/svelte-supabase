b48ac244b77ddbe7e42653957f8fa0af51318c8a
benji78 <47087231+benji78@users.noreply.github.com>
2024-05-06 16:54:20 +0200

    Prettier settings (#12)
    format: change prettier settings

--- src/lib/components/Revenue.svelte
+++ src/lib/components/Revenue.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
-	import { VisLine, VisScatter, VisXYContainer } from "@unovis/svelte";
-	import { color, scatterPointColors, scatterPointStrokeColors } from "./helpers.js";
+  import { VisLine, VisScatter, VisXYContainer } from '@unovis/svelte'
+  import { color, scatterPointColors, scatterPointStrokeColors } from './helpers.js'
 
-	const data = [
-		{ id: 1, revenue: 10400 },
-		{ id: 2, revenue: 14405 },
-		{ id: 3, revenue: 9400 },
-		{ id: 4, revenue: 8200 },
-		{ id: 5, revenue: 7000 },
-		{ id: 6, revenue: 9600 },
-		{ id: 7, revenue: 11244 },
-		{ id: 8, revenue: 26475 },
-	];
-	const x = (d: { revenue: number; id: number }) => d.id;
-	const y = (d: { revenue: number; id: number }) => d.revenue;
+  const data = [
+    { id: 1, revenue: 10400 },
+    { id: 2, revenue: 14405 },
+    { id: 3, revenue: 9400 },
+    { id: 4, revenue: 8200 },
+    { id: 5, revenue: 7000 },
+    { id: 6, revenue: 9600 },
+    { id: 7, revenue: 11244 },
+    { id: 8, revenue: 26475 },
+  ]
+  const x = (d: { revenue: number; id: number }) => d.id
+  const y = (d: { revenue: number; id: number }) => d.revenue
 </script>
 
 <VisXYContainer {data} height="80">
-	<VisLine {x} {y} color={color()} />
-	<VisScatter
-		{x}
-		{y}
-		size={6}
-		color={scatterPointColors}
-		strokeColor={scatterPointStrokeColors}
-		strokeWidth={2}
-	/>
+  <VisLine color={color()} {x} {y} />
+  <VisScatter color={scatterPointColors} size={6} strokeColor={scatterPointStrokeColors} strokeWidth={2} {x} {y} />
 </VisXYContainer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	import '../app.pcss';
+  import '../app.pcss'
 </script>
 
 <div class="flex h-screen w-screen items-center justify-center">
-	<slot />
+  <slot />
 </div>
