39c50eda684d1b80fc91f7be99d07f88c908e507
Hadouin <hadouin.leroy@gmail.com>
2024-06-05 18:40:09 +0200

    logo

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -23,9 +23,11 @@
   import { SearchBar } from '$lib/components/search'
   import { NavBarItem } from '$lib/components/nav-bar-item'
   import { navigating } from '$app/stores'
-  import type { PageData } from './$types'
 
-  export let data: PageData
+  export let data;
+
+  let { supabase } = data
+  $: ({ supabase } = data)
 
   // const session = data.session as Session | null
   const session = true
@@ -48,10 +50,10 @@
 
 <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
   <div class="hidden border-r bg-muted/40 md:block">
-    <div class="flex h-full max-h-screen flex-col gap-2">
+    <div class="flex flex-col h-full max-h-screen gap-2">
       <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
         <a class="flex items-center gap-2 text-xl font-semibold" href="/">
-          <Building2 class="h-6 w-6" />
+          <Building2 class="w-6 h-6" />
           Deep Trading
         </a>
       </div>
@@ -64,12 +66,12 @@
       </div>
     </div>
   </div>
-  <div class="flex h-screen flex-col">
+  <div class="flex flex-col h-screen">
     <header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <Sheet.Root bind:open>
         <Sheet.Trigger asChild let:builder>
           <Button builders={[builder]} class="shrink-0 md:hidden" size="icon" variant="outline">
-            <Menu class="h-5 w-5" />
+            <Menu class="w-5 h-5" />
             <span class="sr-only">Afficher/cacher le menu de navigation</span>
           </Button>
         </Sheet.Trigger>
@@ -77,7 +79,7 @@
           <nav class="grid gap-2 text-lg font-medium">
             <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
               <a class="flex items-center gap-2 text-xl font-semibold" href="/">
-                <Building2 class="h-6 w-6" />
+                <Building2 class="w-6 h-6" />
                 Deep Trading
               </a>
             </div>
@@ -87,7 +89,7 @@
           </nav>
         </Sheet.Content>
       </Sheet.Root>
-      <div class="w-full flex-1">
+      <div class="flex-1 w-full">
         <SearchBar className="md:w-2/3 lg:w-1/2 xl:w-1/3"></SearchBar>
       </div>
       <Dropdown.Root>
@@ -109,7 +111,7 @@
       <Dropdown.Root>
         <Dropdown.Trigger asChild let:builder>
           <Button builders={[builder]} class="rounded-full" size="icon" variant="secondary">
-            <CircleUser class="h-5 w-5" />
+            <CircleUser class="w-5 h-5" />
             <span class="sr-only">Afficher/cacher le menu utilisateur</span>
           </Button>
         </Dropdown.Trigger>
@@ -124,7 +126,9 @@
           <Dropdown.Item>Paramètres</Dropdown.Item>
           <Dropdown.Item>Support</Dropdown.Item>
           <Dropdown.Separator />
-          <Dropdown.Item on:click={() => console.log("logout")}>Déconnexion</Dropdown.Item>
+          <Dropdown.Item on:click={() => {
+            supabase.auth.signOut()
+          }}>Déconnexion</Dropdown.Item>
         </Dropdown.Content>
       </Dropdown.Root>
     </header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+  import { Auth } from '@supabase/auth-ui-svelte'
+  import { ThemeSupa } from '@supabase/auth-ui-shared'
+  import * as Card from '$lib/components/ui/card'
+  import Separator from '$lib/components/ui/separator/separator.svelte'
+
+  export let data
+</script>
+
+<svelte:head>
+  <title>User Management</title>
+</svelte:head>
+
+<div class="flex items-center justify-center w-screen h-screen">
+  <Card.Root class="w-full max-w-sm">
+		<Card.Header>
+			<Card.Title class="flex items-center gap-2 text-2xl">
+				<img src="logoDeepTrading.svg" class="w-12 h-12" alt="Logo">
+				Deep Trading
+			</Card.Title>
+			<Card.Description>Enter your email below to login to your account.</Card.Description>
+		</Card.Header>
+		<Card.Content class="grid gap-4">
+			<Auth
+      supabaseClient={data.supabase}
+      view="magic_link"
+      redirectTo={`${data.url}/auth/callback`}
+      showLinks={false}
+      appearance={{
+        theme: ThemeSupa,
+      }}
+    />
+		</Card.Content>
+	</Card.Root>
+</div>
