c392da6823855f3e7bf077dcfd94bb1b1cfefbb2
Ghislain Demaël <demaelghislain@gmail.com>
2024-06-06 14:13:18 +0200

    Fixed supabase url and anonkey name in supabaseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY as string
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables. Check your .env file.')
