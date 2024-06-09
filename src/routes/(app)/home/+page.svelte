06c5c0660f5d9f15c9205341393589436bbc0c3b
Hadouin <hadouin.leroy@gmail.com>
2024-06-10 00:05:49 +0200

    voir la valeur du portefeuille

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -2,6 +2,7 @@
   import { VisLine, VisXYContainer } from '@unovis/svelte'
   import type { PageData } from './$types'
   import Bots from './Bots.svelte'
+  import * as Card from '$lib/components/ui/card'
 
   export let data: PageData
 
@@ -15,13 +16,23 @@
 
   const x = (d: { value: number; id: number }) => d.id
   const y = (d: { value: number; id: number }) => d.value
+
+  const profit = data.history.profit_loss_pct.slice(-1)[0] * 100
 </script>
 
 <h1>Voir mon portefeuille</h1>
 
-<VisXYContainer data={chartData} height="80">
-  <VisLine {x} {y} />
-</VisXYContainer>
+<Card.Root class="w-96">
+  <Card.Header class="flex flex-row items-center justify-between pb-2 space-y-0">
+    <Card.Title class="text-xl font-bold">${data.history.equity.slice(-1)[0]}</Card.Title>
+    <p class="{profit > 0 ? 'text-green-600' : 'text-red-500'}">{profit > 0 ? '+' : '-'}{profit}%</p>
+  </Card.Header>
+  <Card.Content>
+    <VisXYContainer data={chartData} height="80">
+      <VisLine {x} {y} />
+    </VisXYContainer>
+  </Card.Content>
+</Card.Root>
 
 <pre>{JSON.stringify(data.history, null, 2)}</pre>
 
